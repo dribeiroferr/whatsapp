66ccc2c24983eda5c5da2c1be72fbb17f4121b03
Davi Ferreira <academic.dave.ito@gmail.com>
2022-04-14 13:38:02 -0300

    development environment is already to go

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# whatsapp" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/dribeiroferr/whatsapp.git
-git push -u origin main
