90f70fc09d2e2141accbd89f2512b8b31e7f10b8
Davi Ferreira <academic.dave.ito@gmail.com>
2022-04-19 20:00:52 -0300

    fixing routes.ts

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -29,7 +29,7 @@ router.options('*', cors(options));
  * 
  */
 
-router.get("/", async (req: Request, res: response) => {
+router.get("/", async (req: Request, res: Response) => {
     try {
         return res.json({
             message: "error",
